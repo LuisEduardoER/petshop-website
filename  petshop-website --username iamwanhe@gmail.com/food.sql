8469d4e2a69212c4f1aa3d070464b9e81b6fc18f
iamwanhe <iamwanhe@gmail.com>
2012-12-25 04:26:37 +0000

    Create Table structure for food.

---
+++  petshop-website --username iamwanhe@gmail.com/food.sql
@@ -0,0 +1,29 @@
+-- ----------------------------
+-- Table structure for foot
+-- ----------------------------
+DROP TABLE IF EXISTS `foot`;
+CREATE TABLE `foot` (
+  `id` int(11) NOT NULL auto_increment,
+  `code` varchar(10) collate utf8_bin default NULL,
+  `name_ch` varchar(50) collate utf8_bin NOT NULL,
+  `name_en` varchar(50) collate utf8_bin NOT NULL,
+  `first_char` varchar(1) collate utf8_bin NOT NULL,
+  `comment_ch` text collate utf8_bin,
+  `modify_time` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `status` tinyint(4) default NULL,
+  `isrecommend` int(1) default '0',
+  `isnew` int(1) default '0',
+  `comment_en` text collate utf8_bin,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for foot_collection
+-- ----------------------------
+DROP TABLE IF EXISTS `foot_collection`;
+CREATE TABLE `foot_collection` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `foot_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
