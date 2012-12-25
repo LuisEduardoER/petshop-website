c1cca0e5a70defd3143a48316f10c76078ecbef5
guchaoyangpeter@hotmail.com <guchaoyangpeter@hotmail.com@3455b5d5-b45d-b1bc-ccf4-f33741dec36a>
2012-12-25 04:30:50 +0000

    Create Table structure for user.

---
+++ trunk/ petshop-website --username guchaoyangpeter@hotmail.com/user.sql
@@ -0,0 +1,14 @@
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `account` varchar(20) NOT NULL COMMENT '通行证账号',
+  `password` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `question_id` int(11) NOT NULL COMMENT '密保问题id',
+  `answer` varchar(200) NOT NULL COMMENT '密保答案',
+  `name` varchar(200) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
